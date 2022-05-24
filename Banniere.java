ef1fddc4aebfd695ea94572b15fb2b0eafee0c3d
Haïssous <kayyissa.haissous@etu.u-pec.fr>
2022-05-24 16:15:21 +0200

    nettoyage

--- Banniere.java
+++ Banniere.java
@@ -18,25 +18,27 @@ public class Banniere extends JPanel {
 	}
 
 	//	Méthode pour afficher le nombre de mines restantes
-	public void setMinesLeft(int minesLeft){
+	public void setMinesLeft(int minesLeft) {
 		this.removeAll();
-		this.add(new JLabel("Mines restantes : "+Integer.toString(minesLeft)));
+		this.add(new JLabel("Mines restantes : "+Integer.toString(minesLeft)),BorderLayout.CENTER);
 		this.repaint();
 	}
 
 	//	Méthode pour indiquer au joueur sa Victoire
-	public void setVictoire(){
-		this.add(new Fin("Victoire !   Retour au menu...",this.getSize()));
-		this.repaint();
+	public void setVictoire() {
+		JLabel victoire = new JLabel("Victoire !   Retour au menu...");
+		victoire.setForeground(new Color(0, 22, 236));
+		this.add(victoire,BorderLayout.SOUTH);
   		Timer timerMenu = new Timer(5000, new MenuListener(this.fenetre, this.menu));
   		timerMenu.setRepeats(false);
 		timerMenu.start();
 	}
 
 	//	Méthode pour indiquer au joueur sa Défaite
-	public void setDefaite(){
-		this.add(new Fin("Défaite !   Retour au menu...",this.getSize()));
-		this.repaint();
+	public void setDefaite() {
+		JLabel defaite = new JLabel("Défaite !   Retour au menu...");
+		defaite.setForeground(new Color(0, 22, 236));
+		this.add(defaite,BorderLayout.SOUTH);
 		Timer timerMenu = new Timer(5000, new MenuListener(this.fenetre, this.menu));
   		timerMenu.setRepeats(false);
   		timerMenu.start();
